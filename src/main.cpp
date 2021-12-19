0d7345adcefe78c15e4357b2c6673502bdfbd645
Maciej Krzyżowski <macikrz683@student.polsl.pl>
2021-12-19 18:41:03 +0100

    added inserting method

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ public:
         this->val = 0;
         this->left = nullptr;
         this->right = nullptr;
-        std::cout << "\tCreating a node...\n";
+        //std::cout << "\tCreating a node...\n";
     }
     Node(int _val)
     {
         this->val = _val;
         this->left = nullptr;
         this->right = nullptr;
-        std::cout << "\tCreating a node...\n";
+        //std::cout << "\tCreating a node...\n";
     }
     
     int getVal()
@@ -66,32 +66,101 @@ public:
         {
             if (currNode->left != nullptr)
             {
-                std::cout << "checking left...\n";
                 result = findNode(_val, currNode->left);
             }
             if (result == nullptr && currNode->right != nullptr)
             {
-                std::cout << "checking right...\n";
                 result = findNode(_val, currNode->right);
             }
-            if (result == nullptr)
-                std::cout << "Couldn't find such node!\n";
+            return result;
         }
         else return nullptr;
-
-        return result;
     }
 
-    void insertNode(int _val, std::shared_ptr<Node> currNode = nullptr, int currHeight = 1)
+    bool insertNode(int _val, std::shared_ptr<Node> currNode = nullptr, int currHeight = 2)
     {
+        auto check = [](int x) { return (x+1 != 0 && (x+1 & (x)) == 0); }; // bit manipulation way to check if the number is a (power of 2)-1
+
         if (currNode == nullptr)
             currNode = root;
 
-        this->numNodes = 5;
+        int maxHeight = floor(log2(this->numNodes)) + 1; // this maximum (allowed) height is actually a minimum height of a binary tree with such number of nodes
 
-        int maxHeight = ceil(log2(numNodes)) + 1;
-        if (maxHeight == 1) maxHeight++;
-        std::cout << maxHeight << "\n";
+        maxHeight = check(numNodes) ? maxHeight+1 : maxHeight; // if the number was 3, 7, 15, ... it was breaking the algorithm
+
+        if (maxHeight == 1)
+        {
+            currNode->left = std::shared_ptr<Node>(new Node(_val));
+            this->numNodes++;
+            return true;
+        }
+        if (currHeight > maxHeight) 
+        {
+            return false;
+        }
+        
+        if (currNode->left == nullptr && currHeight <= maxHeight)
+        {
+            currNode->left = std::shared_ptr<Node>(new Node(_val));
+            this->numNodes++;
+            return true;
+        }
+        else if (currNode->right == nullptr && currHeight <= maxHeight)
+        {
+            currNode->right = std::shared_ptr<Node>(new Node(_val));
+            this->numNodes++;
+            return true;
+        }
+        else if (insertNode(_val, currNode->left, currHeight+1))
+        {
+            return true;
+        }
+        else if (insertNode(_val, currNode->right, currHeight+1))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+    void insert(int _val)
+    {
+        std::shared_ptr<Node> temp = nullptr;
+        temp = findNode(_val);
+        if (temp != nullptr)
+        {
+            char c;
+            std::cout << "A node with such value has been found in the tree. Do you want to continue?\n(two separate nodes with that value will be present in the tree) [y/N]";
+            std::cin >> c;
+            if (c == 'y' || c == 'Y') { insertNode(_val); }
+            else return;
+        }
+        else insertNode(_val);
+    }
+
+    std::string spacing(int n)
+    {
+        std::string res = "";
+        for (int i = 0; i < n; i++)
+            res += "  ";
+        return res;
+    }
+    void printTree_helper(std::shared_ptr<Node> node, int level = 0)
+    {
+        if (node == nullptr)
+        {
+            std::cout << "--\n";
+            return;
+        }
+        if (level == 0) std::cout << "root: ";
+        std::cout << node->getVal() << "\n";
+        std::cout << spacing(level) << "  left: ";
+        printTree_helper(node->left, level+1);
+        std::cout << spacing(level) << "  right: ";
+        printTree_helper(node->right, level+1);
+    }
+    void printTree()
+    {
+        printTree_helper(this->root);
     }
     
 };
@@ -99,37 +168,23 @@ public:
 
 int main()
 {
-    std::shared_ptr<Node> root(new Node(5));
-    root->setVal(10);
-    std::cout << root->getVal() << "\n";
-    root->left = std::shared_ptr<Node>(new Node(2));
-    std::cout << root->left->getVal() << "\n";
-    root->right = std::shared_ptr<Node>(new Node(3));
-    std::cout << root->right->getVal() << "\n";
-
-    
     Tree T;
-    //std::cout << T.root->getVal() << "\n";
-    T.root->left = std::shared_ptr<Node>(new Node(2));
-    //std::cout << T.root->left->getVal() << "\n";
-    T.root->right = std::shared_ptr<Node>(new Node(6));
-    //std::cout << T.root->right->getVal() << "\n";
-
-    T.root->right->right = std::shared_ptr<Node>(new Node(7));
-    //std::cout << T.root->right->right->getVal() << "\n";
 
     std::shared_ptr<Node> someNode = nullptr;
 
-    int k;
+    int n, m;
+    for (int i = 0; i < 5; i++)
+    {
+        T.printTree();
+        std::cout << " ~> ";
+        std::cin >> n;
+        T.insertNode(n);
+    }
     std::cout << "look for what number? ";
-    std::cin >> k;
-    someNode = T.findNode(k);
-    /*
-    if (someNode != nullptr)
-        std::cout << someNode->getVal() << "\n";
-    */
-    std::cout << "\n\n";
+    std::cin >> m;
+    someNode = T.findNode(m);
 
-    T.insertNode(8);
+    someNode->setVal(20);
+    T.printTree();
     
 }
