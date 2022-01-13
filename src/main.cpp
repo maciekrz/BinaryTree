c021039bf7c90882ad066ce07a6d2112d66e10d3
Maciej Krzyżowski <macikrz683@student.polsl.pl>
2022-01-13 20:55:58 +0100

    started working on an interactive mode

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,24 @@
 #include "node.cpp"
 #include "tree.cpp"
 
-int main()
+void run(std::string inFileName, std::string outFileName);
+void flags(int argc, char* argv[], std::string* inFileName, std::string* outFileName, bool* r_flag);
+
+int main(int argc, char* argv[])
 {
+    bool r_flag = false;
+    std::string inFile, outFile = "";
+    flags(argc, argv, &inFile, &outFile, &r_flag);
+
+    if (r_flag == false)
+    {
+        std::cout << "\nAvailable flags:\n"
+        << "-i / --input path_to_file  --  specifies the input file (data/input.txt if not used)\n"
+        << "-o / --output path_to_file  --  specifies the output file (data/output.txt if not used)\n"
+        << "-r / --run  --  runs the program\n\n";
+        return 0;
+    }
+
     Tree<int> tree;
 
     tree.insert(10);
@@ -23,4 +39,33 @@ int main()
      *  TO DO:
      *  * SPLIT THE FILE INTO HEADERS
      */
+}
+
+void run(std::string inFileName, std::string outFileName)
+{
+    std::string option = "";
+}
+
+void flags(int argc, char* argv[], std::string* inFileName, std::string* outFileName, bool* r_flag)
+{
+    for (int i = 1; i < argc; i++)
+    {
+        auto argument = std::string(argv[i]);
+        if (argument == "-i" || argument == "--input")
+        {
+            *inFileName = std::string(argv[i+1]);
+            std::cout << "Input data file name: " << *inFileName << "\n";
+        }
+        else if (argument == "-o" || argument == "--output")
+        {
+            *outFileName = std::string(argv[i+1]);
+            std::cout << "Output data file name: " << *outFileName << "\n";
+        }
+        else if (argument == "-r" || argument == "--run")
+        {
+            *r_flag = true;
+        }
+    }
+    std::cout << "Done\n";
+    return;
 }
